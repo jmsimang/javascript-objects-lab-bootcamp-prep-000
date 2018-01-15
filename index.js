70127a9bb2d37c771a8835d1bcad5f97fb582b6c
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 14:38:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, {[key]: value})
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
 
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj)
+  delete newObj.key
+  return newObj
+}
+
+function destructivelyDeleteObjectWithKeyAndValue(obj, key) {
+  delete obj.key
+  return obj
+}
